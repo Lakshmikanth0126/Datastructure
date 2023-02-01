1f1e99f84dbde5d3261903e67d3bf259d7cd5d8a
Lakshmikanth <113410206+Lakshmikanth0126@users.noreply.github.com>
2023-02-01 13:51:29 +0530

    Add files via upload

--- Factorial using recursion.c
+++ Factorial using recursion.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
- 
-int main()
-{
-    int n, i;
-    unsigned long long factorial = 1;
- 
-    printf("Enter a number to find factorial: ");
-    scanf("%d",&n);
- 
-    if (n < 0)
-        printf("Error! Please enter any positive integer number");
- 
-    else
-    {
-        for(i=1; i<=n; ++i)
-        {
-            factorial *= i;          
-        }
-        printf("Factorial of Number %d = %llu", n, factorial);
-    }
- 
-    getch();
+
+int factorial(int n) {
+  if (n == 0)
+    return 1;
+  else
+    return n * factorial(n-1);
 }
+
+int main() {
+  int n, result;
+  
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+  
+  result = factorial(n);
+  printf("Factorial of %d = %d\n", n, result);
+  
+  return 0;
+}
+
